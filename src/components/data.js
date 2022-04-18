fde7358eaa714b258dae872975bc41ff0c79d285
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-04-18 23:06:32 +0100

    Modified README.md

--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,4 @@
-//export let baseURL = "http://localhost:3500"
+//export let baseURL = "http://localhost:3500/"
 export let baseURL = process.env.REACT_APP_API_URL
 export let leagueStartDateURL = baseURL + "leagueStartDate"
 export let teamsURL = baseURL + "teams"
